538ac05cf98b817ac88bb270959e9f593b654f70
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-02 09:23:03 -0400

    assignment complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    count = 0
+    for dwarve in dwarves:
+        count +=1
+        print(f"{count}. {dwarve}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer):
+    return([f"{plane.capitalize()}!"for plane in planeteer])
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(ingredients):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for type in cheese_types:
+        for ingredient in ingredients:
+            if(type == ingredient):
+                return ingredient
